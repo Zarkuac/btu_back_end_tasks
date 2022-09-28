b9b2360496eb7c5b9215cb6f707cf5a3c823d334
ZARKUAC <102588746+Zarkuac@users.noreply.github.com>
2022-09-28 18:41:12 +0500

    Update Task1.php

--- Task1.php
+++ Task1.php
@@ -1,14 +1,14 @@
 <?php
-	$FirstNumber = 5;
-	$SecondNumber = 11;
-	
-	if ($FirstNumber == 0 || $SecondNumber == 0) {
+  $number = readline("Input Number 1: ");
+  $number2 = readline("Input Number 2: ");
+  
+  if ($number == 0 || $number2 == 0) {
 	exit("The Number Must Not Be Zero");
 	}
-	else if ($FirstNumber > 100 || $SecondNumber > 100) {
+	else if ($number > 100 || $number2 > 100) {
 	  echo "Try Again!";
 	}
 	else {
-	  echo $FirstNumber * $SecondNumber;
+	  echo $number2 * $number;
 	}
 ?>
